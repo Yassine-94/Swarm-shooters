e97a9ea373b2640753ebe9e1364fda84458c89f5
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2022-10-09 18:53:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 import sys
 import time
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, TouchSensor
 from pybricks.parameters import Port
+
 
 
 def main():
@@ -14,19 +15,23 @@ def main():
 
     ev3 = EV3Brick()
     # moteur
-    test_motor = Motor(Port.C)
-    test_motor.run_target(500, 180)
-    #test_motor.run_until_stalled(500) tourne à l'infini jusqu'à forcer l'arrêt
 
+    #test_motor.run_until_stalled(500) tourne à l'infini jusqu'à forcer l'arrêt
+    while (True):
+        touch = TouchSensor(Port.S1) #we add a touch sensor
+        if touch.pressed():#if the touch sensor is pressed then...
+            test_motor = Motor(Port.C) #... we define a variable for the motor at port C and...
+            test_motor.run_target(500, 180) #... we make a rotation of the motor because the touch sensor has been pressed
 
     #son
+    """
     ev3.speaker.beep(frequency=1000, duration=500)
     ev3.speaker.beep(frequency=800, duration=200)
     ev3.speaker.beep(frequency=1000, duration=500)
     ev3.speaker.beep(frequency=800, duration=200)
-
+    """
 
     #ev3.speaker.beep()
 
 if __name__ == "__main__" :
-main()
+    main()
