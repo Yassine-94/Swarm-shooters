4fe0715314c259055e62d01c94a27d83625e117b
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2022-11-23 23:45:36 +0100

    Update main_master.py

--- main_master.py
+++ main_master.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3 
 import os
 import sys
 import time
@@ -10,31 +11,38 @@ from threading import Thread
 #Faire une classe Slave Robot en prenant toutes les fonction du dessous
 class Master_Bot():
     def __init__(self):
-        
-        self.hostMACAddress = '169.254.116.31' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+        self.hostAddress = '169.254.162.114' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
         self.port = 1050 # 3 is an arbitrary choice. However, it must match the port used by the client.
         self.backlog = 1
         self.size = 1024
 
     def server_connection(self):
-        s = socket.socket()
-        s.bind((self.hostMACAddress,self.port))
-        s.listen(self.backlog)
-        client, address = s.accept()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.hostAddress,self.port))
+        #s.listen(self.backlog)
+        s.listen()
+        connexion_client, address = s.accept()
 
     def receive_distance(self):
         while 1:
-        
-            data = client.recv(size)
+            data = connexion_client.recv(self.size)
+
             if data:
                 print(data)
                 spkr = Sound()
-
-                spkr.speak('Distance calculated !')
-
-
+                spkr.speak('Target detected')
+                spkr.speak('Distance calculated')
+                data = data.decode('utf8')
+                data = int(data)
+                spkr.speak(str(data))
+                spkr.speak('You can shoot !')
+    
+    def sound(self) :
+        speak = Sound()
+        speak.speak('Without connection')
 
 
 bot = Master_Bot()
+#bot.sound()
 bot.server_connection()
 bot.receive_distance()
