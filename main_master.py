d38cebc1c610fb449371486629a9a9246f7e0d7f
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2022-11-27 21:11:02 +0100

    Update main_master.py

--- main_master.py
+++ main_master.py
@@ -11,21 +11,21 @@ from threading import Thread
 #Faire une classe Slave Robot en prenant toutes les fonction du dessous
 class Master_Bot():
     def __init__(self):
-        self.hostAddress = '169.254.162.114' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+        self.hostAddress = '169.254.148.173' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
         self.port = 1050 # 3 is an arbitrary choice. However, it must match the port used by the client.
         self.backlog = 1
         self.size = 1024
 
     def server_connection(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.hostAddress,self.port))
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.hostAddress,self.port))
         #s.listen(self.backlog)
-        s.listen()
-        connexion_client, address = s.accept()
+        self.s.listen()
+        self.connexion_client, self.address = self.s.accept()
 
     def receive_distance(self):
         while 1:
-            data = connexion_client.recv(self.size)
+            data = self.connexion_client.recv(self.size)
 
             if data:
                 print(data)
@@ -36,10 +36,11 @@ class Master_Bot():
                 data = int(data)
                 spkr.speak(str(data))
                 spkr.speak('You can shoot !')
+                shoot = 'You can shoot'
+                data_shoot = shoot.encode("utf8")
+                self.s.send(data_shoot)
     
-    def sound(self) :
-        speak = Sound()
-        speak.speak('Without connection')
+
 
 
 bot = Master_Bot()
